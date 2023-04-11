9f22bbdbee90094fbce2a5c4aaa141d896f135c3
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-11 15:06:32 +0200

    exercise 1

---
+++ src/main/java/com/spring/e1w1u2/E1W1U2_JavaApplication.java
@@ -0,0 +1,16 @@
+package com.spring.e1w1u2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1W1U2_JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E1W1U2_JavaApplication.class, args);
+		
+		// exercise 1
+		System.out.println("Ciao Mondo!");
+	}
+
+}
