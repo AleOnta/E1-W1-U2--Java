d360aa5a0094ead9a33812b086843f7d599a40e5
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-11 15:06:09 +0200

    initialized the project

---
+++ src/test/java/com/spring/e1w1u2/E1W1U2JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.e1w1u2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1W1U2JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
